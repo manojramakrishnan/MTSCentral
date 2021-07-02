13536f54c812cb505196c7236eaac322b4bd8102
Akhil <akhilmurali662@gmail.com>
2021-07-02 18:10:39 +0530

    added securityconfiguration

---
+++ src/main/java/com/multiplicandin/mts/MTSCentralApplication.java
@@ -0,0 +1,13 @@
+package com.multiplicandin.mts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MTSCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MTSCentralApplication.class, args);
+	}
+	
+}
