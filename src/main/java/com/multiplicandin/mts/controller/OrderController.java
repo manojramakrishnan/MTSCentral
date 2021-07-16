f0fe516ce1fa48e13540722ffbd47043adc6b0b3
Akhil <akhilmurali662@gmail.com>
2021-07-16 17:30:03 +0530

    added order edit

--- src/main/java/com/multiplicandin/mts/controller/OrderController.java
+++ src/main/java/com/multiplicandin/mts/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.multiplicandin.mts.controller;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -11,11 +13,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.multiplicandin.mts.model.Alert;
 import com.multiplicandin.mts.model.Customer;
 import com.multiplicandin.mts.model.CustomerOrder;
+import com.multiplicandin.mts.model.Store;
 import com.multiplicandin.mts.service.AlertService;
 import com.multiplicandin.mts.service.CustomerService;
 import com.multiplicandin.mts.service.OrderService;
@@ -90,4 +94,80 @@ public class OrderController {
         	
     		return modelAndView;
         }
+	@RequestMapping(value="/admin/orders/edit", method = RequestMethod.POST)
+    public ModelAndView editOrder(@RequestParam(name="orderId") String orderId) {
+        ModelAndView modelAndView = new ModelAndView();
+        CustomerOrder customerOrder = orderService.findById(Integer.valueOf(orderId));
+        
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Customer customer = customerService.findCustomerByEmail(auth.getName());
+        Store store=customer.getStore();
+       List<Alert> alerts = alertService.findAllByCustomerId(customer.getId());
+        int alertCount = 0;
+        if(null != alerts) {
+        	alertCount = alerts.size();
+        }
+        modelAndView.addObject("alertCount", alertCount);
+        modelAndView.addObject("alerts", alerts);
+        modelAndView.addObject("customerOrder", customerOrder);
+        modelAndView.addObject("orderDate",customerOrder.getOrderDate());
+        modelAndView.addObject("customerFullName", customer.getName());
+
+        
+
+        modelAndView.setViewName("/admin/edit-order");
+        return modelAndView;
+    }
+	
+	@RequestMapping(value = "/admin/orders/save", method = RequestMethod.POST)
+    public ModelAndView saveOrderDetails(@RequestParam(name="orderId")String orderId,
+                                        @RequestParam(name="orderDate")Date orderDate,
+                                        @RequestParam(name="orderStatus")String orderStatus,
+                                        @RequestParam(name="orderTotal") BigDecimal orderTotal)
+                               
+                                       {
+		
+		ModelAndView modelAndView = new ModelAndView();
+
+        
+         
+         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+         Customer customer = customerService.findCustomerByEmail(auth.getName());
+         
+         
+         CustomerOrder customerOrder = orderService.getOne(Integer.valueOf(orderId));
+         customerOrder.setOrderDate(orderDate);;
+         customerOrder.setOrderStatus(orderStatus);
+         customerOrder.setOrderTotal(orderTotal);
+        
+         orderService.update(customerOrder);
+         modelAndView.addObject("customer", customer);
+         modelAndView.addObject("customerFullName", customer.getName());
+
+         
+
+         modelAndView.setViewName("redirect:/admin/orderlist");
+
+        return modelAndView;
+
+        }
+	
+	@RequestMapping(value="/orders/delete", method = RequestMethod.POST)
+    public ModelAndView deleteOrder (@RequestParam(name="orderId")String orderId) {
+		ModelAndView modelAndView = new ModelAndView();
+        orderService.deleteById(Integer.valueOf(orderId));
+        
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Customer customer = customerService.findCustomerByEmail(auth.getName());
+
+        modelAndView.addObject("customerFullName", customer.getName());
+
+        modelAndView.setViewName("redirect:/admin/orderlist");
+        
+        return modelAndView;
+    }
+
+
+
 }
